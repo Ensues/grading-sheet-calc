d9db86251fb813316eedb4b50ae078c4a9ba2ace
Ensues <168265114+Ensues@users.noreply.github.com>
2024-09-26 07:33:18 +0800

    Add project files.

---
+++ tests/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtbxSub1 = New System.Windows.Forms.TextBox()
+        Me.txtbxSub2 = New System.Windows.Forms.TextBox()
+        Me.txtbxSub3 = New System.Windows.Forms.TextBox()
+        Me.txtbxSub4 = New System.Windows.Forms.TextBox()
+        Me.txtbxSub5 = New System.Windows.Forms.TextBox()
+        Me.txtbxAve = New System.Windows.Forms.TextBox()
+        Me.txtbxRem = New System.Windows.Forms.TextBox()
+        Me.txtbxLowest = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.bttnProcess = New System.Windows.Forms.Button()
+        Me.bttnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Yu Gothic UI", 28.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(110, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(649, 74)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Grading Sheet Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(42, 170)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(165, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "1st subject grade:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(42, 226)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(173, 28)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "2nd subject grade:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(42, 282)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(169, 28)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "3rd subject grade:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(42, 338)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(168, 28)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "4th subject grade:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(46, 393)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(168, 28)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "5th subject grade:"
+        '
+        'txtbxSub1
+        '
+        Me.txtbxSub1.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxSub1.Location = New System.Drawing.Point(226, 167)
+        Me.txtbxSub1.Name = "txtbxSub1"
+        Me.txtbxSub1.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxSub1.TabIndex = 10
+        '
+        'txtbxSub2
+        '
+        Me.txtbxSub2.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxSub2.Location = New System.Drawing.Point(226, 223)
+        Me.txtbxSub2.Name = "txtbxSub2"
+        Me.txtbxSub2.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxSub2.TabIndex = 11
+        '
+        'txtbxSub3
+        '
+        Me.txtbxSub3.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxSub3.Location = New System.Drawing.Point(226, 279)
+        Me.txtbxSub3.Name = "txtbxSub3"
+        Me.txtbxSub3.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxSub3.TabIndex = 12
+        '
+        'txtbxSub4
+        '
+        Me.txtbxSub4.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxSub4.Location = New System.Drawing.Point(226, 338)
+        Me.txtbxSub4.Name = "txtbxSub4"
+        Me.txtbxSub4.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxSub4.TabIndex = 13
+        '
+        'txtbxSub5
+        '
+        Me.txtbxSub5.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxSub5.Location = New System.Drawing.Point(226, 393)
+        Me.txtbxSub5.Name = "txtbxSub5"
+        Me.txtbxSub5.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxSub5.TabIndex = 14
+        '
+        'txtbxAve
+        '
+        Me.txtbxAve.Enabled = False
+        Me.txtbxAve.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxAve.Location = New System.Drawing.Point(617, 167)
+        Me.txtbxAve.Name = "txtbxAve"
+        Me.txtbxAve.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxAve.TabIndex = 15
+        '
+        'txtbxRem
+        '
+        Me.txtbxRem.Enabled = False
+        Me.txtbxRem.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxRem.Location = New System.Drawing.Point(617, 220)
+        Me.txtbxRem.Name = "txtbxRem"
+        Me.txtbxRem.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxRem.TabIndex = 16
+        '
+        'txtbxLowest
+        '
+        Me.txtbxLowest.Enabled = False
+        Me.txtbxLowest.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.txtbxLowest.Location = New System.Drawing.Point(617, 276)
+        Me.txtbxLowest.Name = "txtbxLowest"
+        Me.txtbxLowest.Size = New System.Drawing.Size(126, 34)
+        Me.txtbxLowest.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(446, 170)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 28)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Average:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(446, 223)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 28)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "Remarks:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(446, 276)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(134, 28)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Lowest Grade:"
+        '
+        'bttnProcess
+        '
+        Me.bttnProcess.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.bttnProcess.Location = New System.Drawing.Point(502, 338)
+        Me.bttnProcess.Name = "bttnProcess"
+        Me.bttnProcess.Size = New System.Drawing.Size(202, 48)
+        Me.bttnProcess.TabIndex = 21
+        Me.bttnProcess.Text = "Process Grades"
+        Me.bttnProcess.UseVisualStyleBackColor = True
+        '
+        'bttnClear
+        '
+        Me.bttnClear.Font = New System.Drawing.Font("Yu Gothic UI", 10.0!)
+        Me.bttnClear.Location = New System.Drawing.Point(502, 393)
+        Me.bttnClear.Name = "bttnClear"
+        Me.bttnClear.Size = New System.Drawing.Size(202, 48)
+        Me.bttnClear.TabIndex = 22
+        Me.bttnClear.Text = "Clear"
+        Me.bttnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(868, 510)
+        Me.Controls.Add(Me.bttnClear)
+        Me.Controls.Add(Me.bttnProcess)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtbxLowest)
+        Me.Controls.Add(Me.txtbxRem)
+        Me.Controls.Add(Me.txtbxAve)
+        Me.Controls.Add(Me.txtbxSub5)
+        Me.Controls.Add(Me.txtbxSub4)
+        Me.Controls.Add(Me.txtbxSub3)
+        Me.Controls.Add(Me.txtbxSub2)
+        Me.Controls.Add(Me.txtbxSub1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtbxSub1 As TextBox
+    Friend WithEvents txtbxSub2 As TextBox
+    Friend WithEvents txtbxSub3 As TextBox
+    Friend WithEvents txtbxSub4 As TextBox
+    Friend WithEvents txtbxSub5 As TextBox
+    Friend WithEvents txtbxAve As TextBox
+    Friend WithEvents txtbxRem As TextBox
+    Friend WithEvents txtbxLowest As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents bttnProcess As Button
+    Friend WithEvents bttnClear As Button
+End Class
---
+++ tests/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private ave As Double = 0
+    Private sum As Double = 0
+    Private low As Double = 0
+    Private x As Integer = 0
+    Private y As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles bttnClear.Click
+        txtbxSub1.Clear()
+        txtbxSub2.Clear()
+        txtbxSub3.Clear()
+        txtbxSub4.Clear()
+        txtbxSub5.Clear()
+        txtbxRem.Clear()
+        txtbxLowest.Clear()
+        txtbxAve.Clear()
+    End Sub
+
+    Private Sub bttnProcess_Click(sender As Object, e As EventArgs) Handles bttnProcess.Click
+        sum = Val(txtbxSub1.Text) + Val(txtbxSub2.Text) + Val(txtbxSub3.Text) + Val(txtbxSub4.Text) + Val(txtbxSub5.Text)
+        ave = Val(sum / 5)
+
+        If txtbxSub1.Text < txtbxSub2.Text And txtbxSub1.Text < txtbxSub3.Text And txtbxSub1.Text < txtbxSub4.Text And txtbxSub1.Text < txtbxSub5.Text Then
+            txtbxLowest.Text = txtbxSub1.Text
+        ElseIf txtbxSub2.Text < txtbxSub1.Text And txtbxSub2.Text < txtbxSub3.Text And txtbxSub2.Text < txtbxSub4.Text And txtbxSub2.Text < txtbxSub5.Text Then
+            txtbxLowest.Text = txtbxSub2.Text
+        ElseIf txtbxSub3.Text < txtbxSub1.Text And txtbxSub3.Text < txtbxSub2.Text And txtbxSub3.Text < txtbxSub4.Text And txtbxSub3.Text < txtbxSub5.Text Then
+            txtbxLowest.Text = txtbxSub3.Text
+        ElseIf txtbxSub4.Text < txtbxSub1.Text And txtbxSub4.Text < txtbxSub2.Text And txtbxSub4.Text < txtbxSub3.Text And txtbxSub4.Text < txtbxSub5.Text Then
+            txtbxLowest.Text = txtbxSub4.Text
+        ElseIf txtbxSub5.Text < txtbxSub1.Text And txtbxSub5.Text < txtbxSub2.Text And txtbxSub5.Text < txtbxSub3.Text And txtbxSub5.Text < txtbxSub4.Text Then
+            txtbxLowest.Text = txtbxSub5.Text
+        End If
+
+        If 96 < ave And ave < 100 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Excellent"
+        ElseIf 86 < ave And ave < 95 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Very Good"
+        ElseIf 76 < ave And ave < 85 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "good"
+        ElseIf 66 < ave And ave < 75 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Satisfactory"
+        ElseIf 60 < ave And ave < 65 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Pass"
+        ElseIf 51 < ave And ave < 59 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Conditional Pass"
+        ElseIf 41 < ave And ave < 50 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Conditional Fail"
+        ElseIf 0 < ave And ave < 40 Then
+            txtbxAve.Text = ave
+            txtbxRem.Text = "Fail"
+        ElseIf ave > -0 And 100 < ave Then
+            MsgBox("Pameters not fitted", vbOKOnly + vbObjectError, "Error")
+            Stop
+        End If
+
+
+    End Sub
+End Class
